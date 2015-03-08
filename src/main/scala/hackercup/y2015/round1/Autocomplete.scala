d708b001725159efd8c1d682368effd7de2c9fb8
suicide <suicide@get-it.us>
2015-03-08 17:31:43 +0100

    structure..

---
+++ src/main/scala/hackercup/y2015/round1/Autocomplete.scala
@@ -0,0 +1,26 @@
+package hackercup.y2015.round1
+
+/**
+ * TODO: Comment
+ *
+ * @author Patrick Sy (patrick.sy@get-it.us)
+ */
+object Autocomplete {
+
+  case class Node(value: String, children: Map[String, Node])
+
+  def solve(words: Seq[String]): Int = {
+
+    def buildTrie(words: Seq[String]): Node = {
+      val sortedWords = words.sorted
+
+
+
+
+    }
+
+
+    0
+  }
+
+}
